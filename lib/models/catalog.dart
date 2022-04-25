fceec7808f6ecb10ba11216a4e97fdca1b2919ac
Climax Dev <shahzaibclimaxcode12@gmail.com>
2022-04-25 10:24:33 +0500

    completed day13

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -8,16 +8,28 @@ class Item {
 
   Item(
       {required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
+
+  factory Item.fromMap(Map<String, dynamic> map){
+    return Item(
+      id: map["id"],
+      name: map["name"],
+      desc: map["desc"],
+      price: map["price"],
+      color: map["color"],
+      image: map["image"]
+    );
+  }
+
+  toMap() =>{
+    "id" : id,
+    "name" : name,
+    "desc" : desc,
+    "price" : price,
+    "color" : color,
+    "image" : image
+  };
 }
 
 class CatalogModel {
-  static final items = [
-    Item(id: 1,
-        name: "iPhone 12 Pro",
-        desc: "Apple iPhone 12th generation",
-        price: 999,
-        color: "#33505a",
-        image : "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MHL83?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1601149134000"
-    )
-  ];
+  static List<Item> items = [];
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,30 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:test_flutter/models/catalog.dart';
 import 'package:test_flutter/widgets/drawer.dart';
+import 'dart:convert';
 
 import '../widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
-  final int days = 30;
-
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final int days = 30;
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async{
+    await Future.delayed(const Duration(seconds: 3));
+    final catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productData = decodedData["products"];
+
+    CatalogModel.items = List.from(productData).map<Item>((item) => Item.fromMap(item)).toList();
+    setState(() {});
+
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(10, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Catalog App"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-            itemCount: dummyList.length,
-            itemBuilder: (context, index){
-              return ItemWidget(item: dummyList[index]);
-            }),
+        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty) ? ListView.builder(
+            itemCount: CatalogModel.items.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(item: CatalogModel.items[index]);
+            }) : const Center(child: CircularProgressIndicator(),),
       ),
-      drawer:  const MyDrawer(),
+      drawer: const MyDrawer(),
     );
   }
 }
